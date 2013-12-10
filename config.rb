090b7c298df6f49ccf5ea9bdfd5fa5e3817d72b3
Jamie Schembri <jamie@schembri.me>
2013-12-10 19:52:24 +0100

    Cleaned some stuff from config.rb.

--- config.rb
+++ config.rb
@@ -2,15 +2,11 @@
 # General settings.
 ######################################################################
 
-# Meta
+# Meta.
 set :meta, {
   title: 'Foo',
   url: 'http://foo.com',
 }
-
-# Loads IE8 compatible versions of normalize & jQuery if true (1.x).
-# Otherwise, loads IE8 incompatible versions (2.x).
-set :ie8_support, false
 
 # Syntax highlighting.
 activate :syntax
@@ -35,15 +31,12 @@ set :fonts_dir, 'assets/fonts'
 set :partials_dir, 'partials'
 
 after_configuration do
-# Add bower's directory to sprockets asset path.
+  # Add bower's directory to sprockets asset path.
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
 
-# Slim's default format is xhtml. Why oh why?
-Slim::Engine.default_options[:format] = :html5
-
-# Ignores
+# Ignores.
 ignore '/calendar.html'
 
 
@@ -55,27 +48,16 @@ Time.zone = "Amsterdam"
 
 activate :blog do |blog|
   blog.prefix = 'blog'
-  blog.permalink = ':year/:title.html'
-  # blog.sources = ":year-:month-:day-:title.html"
-  blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
+  blog.permalink = '{year}/{title}.html'
+  blog.taglink = "tags/{tag}.html"
   blog.default_extension = ".md"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
 
   blog.paginate = true
   blog.per_page = 10
-  # blog.page_link = "page/:num"
 end
 
 # Enable XML feed. Don't forget to edit feed.xml.builder first.
-# page "/feed.xml", :layout => false
+# page "/feed.xml", layout: false
 
 
 ######################################################################
